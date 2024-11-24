307181b6527c37873788d876f2586aa6794428ab
gangeshpathak <mr.pathak00122@gmail.com>
2024-11-24 23:47:23 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Mockman from "mockman-js";
+import { useEffect } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useProducts } from "./Contexts/ProductsProvider";
+
+import {
+  Cart,
+  Checkout,
+  ErrorPage,
+  Footer,
+  Header,
+  Home,
+  Loading,
+  Products,
+  Profile,
+  SingleProduct,
+  Success,
+  Wishlist,
+} from "./Pages";
 
 function App() {
+  const { fetchCategoriesAgain, fetchProductsAgain } = useProducts();
+
+  useEffect(() => {
+    fetchProductsAgain();
+    fetchCategoriesAgain();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Loading />
+      <Header />
+      <ToastContainer />
+      <Routes>
+        <Route path="/mockman" element={<Mockman />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/api/products/:prodId" element={<SingleProduct />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
